3f9029054768ed5e0640347486dc59a7130756c8
jacobzouzou <jacob.zouzou@gmail.com>
2020-09-22 11:05:08 +0200

    rename test file with "test" in name

--- src/math.test.js
+++ src/math.test.js
@@ -1,21 +1,26 @@
 const res = 4 ** 2;
 // console.log(Math.pow(4,2));
-console.log(res);
+// console.log(res);
 
 const sum = (a, b) => a + b;
 const mul = (a, b) => a * b;
 const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
 
-test("Adding 1 + 1 equals 2", () => {
-  expect(sum(1, 1)).toBe(2);
-});
-test("Multiplying 1 * 1 equals 1", () => {
-  expect(mul(1, 1)).toBe(1);
-});
-test("Subtracting 1 - 1 equals 0", () => {
-  expect(sub(1, 1)).toBe(0);
-});
-test("Dividing 1 / 1 equals 1", () => {
-  expect(div(1, 1)).toBe(1);
-});
+describe("My tests", ()=>{
+  // beforeAll(()=>{
+  //   console.log(".....")
+  // })
+  test("Adding 1 + 1 equals 2", () => {
+    expect(sum(1, 1)).toBe(2);
+  });
+  test("Multiplying 1 * 1 equals 1", () => {
+    expect(mul(1, 1)).toBe(1);
+  });
+  test("Subtracting 1 - 1 equals 0", () => {
+    expect(sub(1, 1)).toBe(0);
+  });
+  test("Dividing 1 / 1 equals 1", () => {
+    expect(div(1, 1)).toBe(1);
+  });
+})
