2c3b8f319f80bec91af1611318acdd756f5cd503
jacobzouzou <jacob.zouzou@gmail.com>
2020-09-26 17:09:47 +0200

    update promise chaining function

--- src/chainingPromise.js
+++ src/chainingPromise.js
@@ -1,44 +1,52 @@
+
+// const path = require("path");
+// const fs=require("fs")
+
 //web server
-// const path = require('path');
 const express = require("express");
 const app = express();
-app.get('/', (req, res) =>{
-  //  res.type("json");
-   res.json({name:"Jacob"});
-  }
-);
-app.listen(3000, () => console.log('Server ready: listen to http://localhost:3000'))
+app.get("/", (req, res) => {
+  res.json({ name: "Jacob" });
+});
 
-//Fetch
+const users= require("./data/users.json");
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.listen(3000, () => console.log("Server ready: http://localhost:3000"));
+
+//fetch
 const fetch = require("node-fetch");
 
-const statusPromise = response => {
+const statusPromise = (response) => {
   if (response.status >= 200 && response.status < 300) {
     return Promise.resolve(response);
   } else {
     return Promise.reject(new Error(response.statusText));
   }
-}
-//json() return a promise
-const responseJson = response => response.json();
+};
+const responseJson = (response) => response.json(); //return a promise
 
-//url must http protocol so, file must be serve  from  web server: / from root
+//url must use http protocol so, file must be serve  from  web server: /
 fetch("http://localhost:3000")
   .then(statusPromise)
   .then(responseJson)
-  .then(data => {
+  .then((data) => {
     console.log(`Object entrees : ${Object.entries(data)}`);
     console.log(`Object values : ${Object.values(data)}`);
     console.log(`Object Json string: ${JSON.stringify(data)}`);
-  }).catch(error => {
+  })
+  .catch((error) => {
     console.log(`Failed: ${error}`);
   });
 
-// fetch("/users.json")
-//   .then(statusPromise)
-//   .then(responseJson)
-//   .then(data => {
-//     console.log(`Json response: ${JSON.stringify(data)}`);
-//   }).catch(error => {
-//     console.log(`Failed: ${error}`);
-//   });
+fetch("http://localhost:3000/users")
+  .then(statusPromise)
+  .then(responseJson)
+  .then((data) => {
+    console.log(`Json response: ${JSON.stringify(data)}`);
+  })
+  .catch((error) => {
+    console.log(`Failed: ${error}`);
+  });
